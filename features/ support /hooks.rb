e062ec3295112be4210bc962ef6c4ed2b3b30701
rajann44 <rajann44@gmail.com>
2021-07-08 21:59:24 +0530

    record failed ruby test on mac

---
+++ features/ support /hooks.rb
@@ -0,0 +1,26 @@
+Before do |scenario|
+
+    #
+    # Browser browser
+    #
+    #@browser_name          = :firefox
+    Webdrivers.install_dir = 'webdrivers_bin'
+    recording_name         = scenario.name.gsub(/\s/, '-').downcase
+    @browser               = Watir::Browser.new
+  
+    #
+    # Start recording
+    #
+    #window_title = ScreenRecorder::Titles.fetch(@browser_name).first
+    @recorder = ScreenRecorder::Desktop.new(output: recording_name +".mkv")
+    @recorder.start
+  end
+  
+  #
+  # Stop recording and close browser
+  #
+  After do |scenario|
+    @recorder.stop
+    @browser.quit
+    #@recorder.discard if scenario.passed?
+  end
